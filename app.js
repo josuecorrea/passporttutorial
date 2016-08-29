b2c3c2b2eaf4d80d8df50b0adda0494d08088e94
josuecorrea <josuejtc@gmail.com>
2016-08-28 21:17:49 -0300

    views

--- app.js
+++ app.js
@@ -22,6 +22,60 @@ app.set('views', path.join(__dirname, 'views'));
 app.engine('handlebars', exphbs({defaultLayout:'layout'}));
 app.set('view engine', 'handlebars');
 
+//BodyParser Middleware
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false}));
 app.use(cookieParser());
+
+//set static folder
+app.use(express.static(path.join(___dirname, 'public')));
+
+//Express session
+app.use(session({
+    secret:'secret',
+    saveUninitialized: true,
+    resave: true
+}));
+
+app.use(expressValidator({
+    errorFormatter: function(param, msg, value){
+        var namespace = param.split('.')
+        , root = namespace.shift()
+        , formParam = root;
+
+        while(namespace.length){
+            formParam += '[' + namespace.shift() + ']';
+        }
+        return {
+            param : formParam,
+            msg : msg,
+            value : value
+        };
+    }
+}));
+
+//connect flash
+app.use(flash());
+
+//Global Vars
+app.use(function(req, res, next){
+    res.locals.success_msg = req.flash('success_msg');
+    res.locals.error_msg = req.flash('error_msg');
+    res.locals.error = req.flash('error');
+    next();
+});
+
+app.use('/', routes);
+app.use('/users', users);
+
+app.set('port', (process.env.PORT || 3000));
+
+app.listen(app.get('port'), function(){
+    console.log('Server started on port '+ app.get('port'));
+});
+
+
+
+
+
+
